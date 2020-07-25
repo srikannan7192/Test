95df37378ff8dad238d2c2b824be1077f8b8ed72
Ubuntu <ubuntu@ip-172-31-45-238.us-east-2.compute.internal>
2020-07-25 12:05:11 +0000

    change one line

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+i<?php 
 		include_once('assets/includes/_settings.php');
 		$site['page']['title'] = 'MASTER';
 		//$site['page']['articletitle'] = '';
@@ -10,7 +10,7 @@
 ?>
 <!-- START PAGE CONTENT -->
 <h1>-----------Master Branch---------</h1>
-<h1>Testtt</h1>
+<h1>welcom to all </h1>
 <p>
 You’ll find EWU in Chicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
 <br/><br/>
