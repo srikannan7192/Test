dd9fb57503b97fe6c30eabce359a7395fc2609a6
srikannan7192 <68687905+srikannan7192@users.noreply.github.com>
2020-08-24 16:25:51 +0530

    update

--- index.php
+++ index.php
@@ -11,11 +11,12 @@
 <!-- START PAGE CONTENT -->
 
 <h1> Master Page </h1>
+</h1>devops cloud camp </h1>
 <h1>welcome to all</h1>
 
 
 <p>
-You’ll find EWU in Chicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
+You’ll find EWU in Cicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
 <br/><br/>
 EWU is one of the most cost-effective education institutions in the regions – coming in 5th for the lowest average graduate student debt in a survey of more than 1000 public and private colleges by the New York Times
 </p>
